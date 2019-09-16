e4c25791ffefcda819f27a8a5bc880ffb83b313d
関口大地 <>
2019-09-16 18:10:45 +0900

    feat_tweet_destroy

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -47,11 +47,7 @@ class TweetsController < ApplicationController
   # DELETE /tweets/1
   # DELETE /tweets/1.json
   def destroy
-    @tweet.destroy
-    respond_to do |format|
-      format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+     @tweet.destroy
   end
 
     private
